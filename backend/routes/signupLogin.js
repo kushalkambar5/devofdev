90b7ea0b4346188eabf7ab24f19ce1e00f6d84f5
kushalkambar5 <kushalkambar07@gmail.com>
2026-01-02 22:18:57 +0530

    session auth half done

--- backend/routes/signupLogin.js
+++ backend/routes/signupLogin.js
@@ -1,17 +1,56 @@
 const express = require("express");
 const router = express.Router();
+const session = require("express-session");
+const bcrypt = require("bcrypt");
+const User = require('./../models/user');
 
-router.get('./signup', async(req,res)=>{
+//Middleware
+router.use(express.json());
 
-});
+router.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave:false,
+    saveUninitialized:false,
 
+    cookie:{
+        httpOnly:true,
+        secure:true,
+        sameSite:"strict",
+        maxAge: 1000 * 60 * 60 * 1000
+    }
+}));
+
+//Auth Middleware
+const isAuth = ()=>{
+    if(req.session.userID) return next();
+    return res.status(401).json({message: "Unauthorized"});
+};
+
+
+//Signup
 router.post('./signup', async(req,res)=>{
-    
+
+    //check username exists or not
+    const newUser = new User(req.body);
+    const response = await newUser.save();
+    console.log("Data saved")
+
+    const {email, password} = req.body;
+
+    if(!email || !password)
+    return res.status(400).json({ message: "Missing fields" });
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    try{
+        const user = await UserActivation.create({email, password: hashedPassword});
+        res.json({message:"User registered", userId: user.id});
+    } catch(err){
+        return res.status(400).json({message: "User already exists"});
+    }
 });
 
-router.get('./login', async(req,res)=>{
-    
-});
+
 
 router.post('./login', async(req,res)=>{
     
--- backend/server.js
+++ backend/server.js
@@ -2,16 +2,19 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const signupLoginRoutes = require("./routes/signupLogin");
+const users = require('./routes/users');
 require("dotenv").config();
-const db = require('./db')
+const db = require('./db');
 app.use(express.json());
 
-
+app.use('/users', users);
 app.use('/', signupLoginRoutes);
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
-})
+  const newUser = new User(req.body);
+    db.addListener
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${process.env.PORT || 3000}`)
